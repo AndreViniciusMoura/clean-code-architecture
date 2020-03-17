e0e3f07f46891361bea5494cc74d8b14648d53c0
andre.vinicius.moura@outlook.com <andre.vinicius.moura@outlook.com>
2020-03-16 21:29:15 -0300

    feat: Adiciona o padrao repository ao projeto.

---
+++ src/ApplicationCore/ApplicationCore/Interfaces/Repositories/IBaseRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace ApplicationCore.Interfaces.Repositories
+{
+    public interface IBaseRepository<TEntity> where TEntity : class
+    {
+        TEntity Adicionar(TEntity entity);
+
+        void Atualizar(TEntity entity);
+
+        IEnumerable<TEntity> ObterTodos();
+
+        TEntity ObterPorId(int id);
+
+        IEnumerable<TEntity> Buscar(Expression<Func<TEntity, bool>> predicado);
+
+        void Remover(TEntity entity);
+    }
+}
